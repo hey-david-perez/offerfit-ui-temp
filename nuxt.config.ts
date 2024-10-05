51985d2d9862def2559ee5731ade0bf7b1bad9a6
hey-david-perez <david.perez@offerfit.ai>
2024-10-04 19:38:45 -0500

    component install

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/storybook"],
+  compatibilityDate: "2024-10-04",
 })
