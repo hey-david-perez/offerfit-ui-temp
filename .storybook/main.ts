b95181c7d04780dbacce401557b28da902c1862b
hey-david-perez <david.perez@offerfit.ai>
2024-10-04 23:11:18 -0500

    some plugins

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,14 +2,15 @@ import type { StorybookConfig } from '@storybook-vue/nuxt'
 
 const config: StorybookConfig = {
   stories: [
-    '../stories/**/*.mdx',
-    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
-    '../components/**/*.stories.ts',
+    '../**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@storybook/addon-docs',
+    '@storybook/addon-viewport',
+    '@storybook/addon-a11y',
   ],
   framework: {
     name: '@storybook-vue/nuxt',
